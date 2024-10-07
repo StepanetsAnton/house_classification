c5e7d0923faa61adf7d9350d2af9bfbf868cee7e
anton_stepanets <anton.stepanets15@gmail.com>
2024-10-07 14:41:10 +0200

    splitting the data into train and test sets

--- House Classification/task/solution.py
+++ House Classification/task/solution.py
@@ -1,10 +1,13 @@
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('house_class.csv')
-print(df.shape[0])
-print(df.shape[1])
-print(df.isna().any().any())
-print(df.Room.max())
-print(round(df.Area.mean(),1))
-print(df['Zip_loc'].nunique())
+
+X = df[['Area', 'Room', 'Lon', 'Lat', 'Zip_area', 'Zip_loc']]
+y = df['Price']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=X['Zip_loc'].values)
+
+zip_loc_counts = X_train['Zip_loc'].value_counts().to_dict()
+
+print(zip_loc_counts)
