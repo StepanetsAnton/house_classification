4f7d11b54041ac40fef0099762d4a5431173c79b
anton_stepanets <anton.stepanets15@gmail.com>
2024-10-10 14:22:01 +0200

    classification reports for encoders

--- House Classification/task/solution.py
+++ House Classification/task/solution.py
@@ -5,6 +5,7 @@ from sklearn.preprocessing import OrdinalEncoder
 from category_encoders import TargetEncoder
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
 
 df = pd.read_csv('house_class.csv')
 
@@ -13,55 +14,75 @@ y = df['Price']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=X['Zip_loc'].values)
 
-encoder = OneHotEncoder(drop='first', sparse_output=False)
-encoder.fit(X_train[['Zip_area', 'Zip_loc', 'Room']])
+model_params = {
+    'criterion': 'entropy',
+    'max_features': 3,
+    'splitter': 'best',
+    'max_depth': 6,
+    'min_samples_split': 4,
+    'random_state': 3
+}
 
-X_train_transformed = pd.DataFrame(encoder.transform(X_train[['Zip_area', 'Zip_loc', 'Room']]), index=X_train.index)
-X_test_transformed = pd.DataFrame(encoder.transform(X_test[['Zip_area', 'Zip_loc', 'Room']]), index=X_test.index)
+onehot_encoder = OneHotEncoder(drop='first', sparse_output=False)
+onehot_encoder.fit(X_train[['Zip_area', 'Zip_loc', 'Room']])
 
-X_train_final = X_train[['Area', 'Lon', 'Lat']].join(X_train_transformed)
-X_test_final = X_test[['Area', 'Lon', 'Lat']].join(X_test_transformed)
+X_train_onehot = pd.DataFrame(onehot_encoder.transform(X_train[['Zip_area', 'Zip_loc', 'Room']]), index=X_train.index)
+X_test_onehot = pd.DataFrame(onehot_encoder.transform(X_test[['Zip_area', 'Zip_loc', 'Room']]), index=X_test.index)
 
-X_train_final.columns = X_train_final.columns.astype(str)
-X_test_final.columns = X_test_final.columns.astype(str)
+X_train_onehot.columns = X_train_onehot.columns.astype(str)
+X_test_onehot.columns = X_test_onehot.columns.astype(str)
+
+X_train_onehot_final = X_train[['Area', 'Lon', 'Lat']].join(X_train_onehot)
+X_test_onehot_final = X_test[['Area', 'Lon', 'Lat']].join(X_test_onehot)
+
+model_onehot = DecisionTreeClassifier(**model_params)
+model_onehot.fit(X_train_onehot_final, y_train)
+y_pred_onehot = model_onehot.predict(X_test_onehot_final)
+
+report_onehot = classification_report(y_test, y_pred_onehot, output_dict=True)
+f1_onehot = report_onehot['macro avg']['f1-score']
+print(f"OneHotEncoder:{round(f1_onehot, 2)}")
 
 
 ordinal_encoder = OrdinalEncoder()
 ordinal_encoder.fit(X_train[['Zip_area', 'Zip_loc', 'Room']])
 
-X_train_encoded = pd.DataFrame(ordinal_encoder.transform(X_train[['Zip_area', 'Zip_loc', 'Room']]),
+X_train_ordinal = pd.DataFrame(ordinal_encoder.transform(X_train[['Zip_area', 'Zip_loc', 'Room']]),
                                columns=['Zip_area_encoded', 'Zip_loc_encoded', 'Room_encoded'],
                                index=X_train.index)
 
-X_test_encoded = pd.DataFrame(ordinal_encoder.transform(X_test[['Zip_area', 'Zip_loc', 'Room']]),
+X_test_ordinal = pd.DataFrame(ordinal_encoder.transform(X_test[['Zip_area', 'Zip_loc', 'Room']]),
                               columns=['Zip_area_encoded', 'Zip_loc_encoded', 'Room_encoded'],
                               index=X_test.index)
 
-X_train_final = X_train[['Area', 'Lon', 'Lat']].join(X_train_encoded)
-X_test_final = X_test[['Area', 'Lon', 'Lat']].join(X_test_encoded)
+X_train_ordinal.columns = X_train_ordinal.columns.astype(str)
+X_test_ordinal.columns = X_test_ordinal.columns.astype(str)
+
+X_train_ordinal_final = X_train[['Area', 'Lon', 'Lat']].join(X_train_ordinal)
+X_test_ordinal_final = X_test[['Area', 'Lon', 'Lat']].join(X_test_ordinal)
+
+model_ordinal = DecisionTreeClassifier(**model_params)
+model_ordinal.fit(X_train_ordinal_final, y_train)
+y_pred_ordinal = model_ordinal.predict(X_test_ordinal_final)
+
+report_ordinal = classification_report(y_test, y_pred_ordinal, output_dict=True)
+f1_ordinal = report_ordinal['macro avg']['f1-score']
+print(f"OrdinalEncoder:{round(f1_ordinal, 2)}")
+
 
 target_encoder = TargetEncoder(cols=['Zip_area', 'Room', 'Zip_loc'])
 target_encoder.fit(X_train, y_train)
 
-X_train_encoded = target_encoder.transform(X_train)
-X_test_encoded = target_encoder.transform(X_test)
+X_train_target = target_encoder.transform(X_train)
+X_test_target = target_encoder.transform(X_test)
 
+X_train_target.columns = X_train_target.columns.astype(str)
+X_test_target.columns = X_test_target.columns.astype(str)
 
+model_target = DecisionTreeClassifier(**model_params)
+model_target.fit(X_train_target, y_train)
+y_pred_target = model_target.predict(X_test_target)
 
-
-model = DecisionTreeClassifier(
-    criterion='entropy',
-    max_features=3,
-    splitter='best',
-    max_depth=6,
-    min_samples_split=4,
-    random_state=3
-)
-
-model.fit(X_train_encoded, y_train)
-
-y_pred = model.predict(X_test_encoded)
-
-accuracy = accuracy_score(y_test, y_pred)
-
-print(round(accuracy, 4))
+report_target = classification_report(y_test, y_pred_target, output_dict=True)
+f1_target = report_target['macro avg']['f1-score']
+print(f"TargetEncoder:{round(f1_target, 2)}")
